c1ae7b3cc3014c329f15c43021fe353be50cf510
Daniel Schnabel <daniel@solarwinds.cloud>
2018-11-08 17:25:37 -0800

    AO-6783 remove check for env var since it's useless

--- bin/appoptics.sh
+++ bin/appoptics.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -e
 
-if [ -z "export | grep APPOPTICS_SERVICE_KEY" ]; then
-    echo "Please set the environment variable APPOPTICS_SERVICE_KEY"
-    exit 1
-fi
-
 curl -sSO https://files.appoptics.com/appoptics-php.sh
 bash appoptics-php.sh --service-key=$APPOPTICS_SERVICE_KEY
