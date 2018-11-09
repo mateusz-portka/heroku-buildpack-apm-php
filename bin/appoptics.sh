3d09f91eee9ff04e8fa28a8787a804b5f36c7ce9
Daniel Schnabel <daniel@solarwinds.cloud>
2018-11-09 10:00:08 -0800

    AO-6783 try config var check again

--- bin/appoptics.sh
+++ bin/appoptics.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 
+if [ -z "${APPOPTICS_SERVICE_KEY:-}" ]; then
+    echo "Error: APPOPTICS_SERVICE_KEY is not set"
+    exit 1
+fi
+
 curl -sSO https://files.appoptics.com/appoptics-php.sh
-bash appoptics-php.sh --service-key=$APPOPTICS_SERVICE_KEY
+bash appoptics-php.sh --service-key=${APPOPTICS_SERVICE_KEY:-}
