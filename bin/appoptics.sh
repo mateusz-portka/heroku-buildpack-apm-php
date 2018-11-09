7cf2f9159dfbbe9b6eaeb2719ea3f25cf2a3bb55
Daniel Schnabel <daniel@solarwinds.cloud>
2018-11-08 17:19:25 -0800

    AO-6783 added check for env var

--- bin/appoptics.sh
+++ bin/appoptics.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 
+if [ -z "$APPOPTICS_SERVICE_KEY" ]; then
+    echo "Please set the environment variable APPOPTICS_SERVICE_KEY"
+    exit 1
+fi
+
 curl -sSO https://files.appoptics.com/appoptics-php.sh
 bash appoptics-php.sh --service-key=$APPOPTICS_SERVICE_KEY
